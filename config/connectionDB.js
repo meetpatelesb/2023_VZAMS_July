0ffd48a024f2b6451da55de01850a16a086d000f
siddharth <pnmewada@gmal.com>
2023-03-20 19:22:07 +0530

    23DEV043 connection file change add commit

--- config/connectionDB.js
+++ config/connectionDB.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "vzams_demo",
+    database: "twitter",
   });
 
   module.exports = connection;
