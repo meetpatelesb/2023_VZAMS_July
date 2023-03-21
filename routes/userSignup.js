6c129a8202ec07581ab376c9a910a343bcb9040a
siddharth <pnmewada@gmal.com>
2023-03-21 12:26:55 +0530

    23DEV043 signup ui's usersignup routes file add try catch commit

--- routes/userSignup.js
+++ routes/userSignup.js
@@ -13,7 +13,7 @@ router.use(sessions({
 }));
 
 //PUBLIC ROUTE
-var queryExecute = util.promisify(connection.query).bind(connection)
+var queryExecute = util.promisify(connection.query).bind(connection);
 router.post("/signup",async (req,res) => {
     var userData = [];
     var body = req.body;
@@ -29,16 +29,21 @@ router.post("/signup",async (req,res) => {
     var query = `INSERT INTO user_master (user_username, user_email, user_password,user_activation , user_dob) VALUES
     ('${userData[4]}','${userData[1]}', '${newPsw}', '1','${userData[2]}')`;
     console.log(query);
-    var result = await queryExecute(query);
-
-    console.log(result);
-    if(result)
-    {
-        req.session.email = userData[1];
-        console.log(req.session.email);
+    try{
+        var result = await queryExecute(query);
+        console.log(result);
+        if(result)
+        {
+            req.session.email = userData[1];
+            console.log(req.session.email);
+        }
     }
-    
-    req.session.destroy();
+    catch(err)
+    {
+        console.error("Executing Error!");
+
+    }
+
 })
 
 
@@ -46,16 +51,48 @@ router.post("/valid_email" , async (req , res)=>{
 var userEmail = req.body.email;
 
 var query = `select user_email from user_master where user_email = '${userEmail}'`;
-var result = await queryExecute(query);
-console.log(result);
-if(result.length != 0)
-{  
-    var email = result[0].user_email;
-    console.log(email);
-    res.json({
-        email
-    })
+console.log(query);
+
+try{
+    var result = await queryExecute(query);
+        console.log(result);
+        if(result.length != 0)
+        {  
+            var email = result[0].user_email;
+            console.log(email);
+            res.json({
+                email
+            })
+        }
+}
+catch(err)
+{
+    console.error("Executing Error!");
+
 }
 
+})
+
+router.post("/valid_username", async (req ,res)=>{
+    var userName = req.body.username;
+
+    var query = `select user_username from user_master where user_username = '${userName}'`;
+    console.log(query);
+    try{
+        var result = await queryExecute(query);
+        if(result.length != 0)
+        {  
+            var username = result[0].user_username;
+            console.log(username);
+            res.json({
+                username
+            })
+        }
+    }
+    catch(err)
+    {
+        console.error("Executing Error!");
+    }
+    
 })
 module.exports = router;
