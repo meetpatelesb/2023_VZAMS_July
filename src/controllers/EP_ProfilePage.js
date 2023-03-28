1fc64b32fb8f974f70f28171af83112178c0d95c
Aditya <thakoraditya67@gmail.com>
2023-03-28 09:03:22 +0530

    23DEV043 Small changes in this files

--- src/controllers/EP_ProfilePage.js
+++ src/controllers/EP_ProfilePage.js
@@ -7,12 +7,11 @@ let page_profilePage = async(req, res) => {
 
     if (user_id) {
        
-        let sql = `select user_username,profile_name,profile_bio,profile_following,profile_followers,profile_location
-         from profile_master om 
+        let sql = `select * from profile_master pm 
          join user_master um
-         on om.user_id = um.user_id
-         where om.user_id = ${user_id}
-`
+         on pm.user_id = um.user_id
+         where pm.user_id = ${user_id}
+`;
       
         let get_profile = await queryExecute(sql);
         res.render('../src/views/userprofile', { get_profile });
